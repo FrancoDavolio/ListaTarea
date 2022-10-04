c9db748f4b2b6a270e215e55ef82424cd5ee17e9
francoDavolio <francodavolio12@gmail.com>
2022-10-04 19:57:48 -0300

    se creo el arreglo para guardar las tareas en el componente formulario

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useSyncExternalStore } from "react";
 import { Form, Button } from "react-bootstrap";
 import ListaTarea from "./ListaTarea";
+import { useState } from "react";
 
 const FormularioTarea = () => {
+  const [tarea, setTarea] = useState("");
+  const [arregloTareas, setArregloTareas] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Desde handleSubmit");
+    setArregloTareas([...arregloTareas, tarea]);
+    setTarea("");
+  };
+
   return (
     <div>
-      <Form>
-        <Form.Group className="mb-3 d-flex" controlId="formBasicEmail">
-          <Form.Control type="text" placeholder="Ingrese una tarea" />
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3 d-flex">
+          <Form.Control
+            type="text"
+            placeholder="Ingrese una tarea"
+            onChange={(e) => setTarea(e.target.value)}
+            value={tarea}
+          />
           <Button variant="primary" type="submit">
             Enviar
           </Button>
